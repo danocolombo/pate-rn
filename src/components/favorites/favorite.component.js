f21380bc76d8c6d1dca1b4a7550b9303706f0d1b
danocolombo@google.com <GR0mans1212!>
2021-06-15 15:40:26 -0400

    fav on click attempt

--- src/components/favorites/favorite.component.js
+++ src/components/favorites/favorite.component.js
@@ -6,18 +6,32 @@ import { FavoritesContext } from "../../services/favorites/favorites.context";
 
 const FavoriteButton = styled(TouchableOpacity)`
   position: absolute;
-  top: 10px;
-  right: 10px;
+  top: 3px;
+  right: 5px;
   z-index: 9;
 `;
 
-export const Favorite = () => {
+export const Favorite = ({ event }) => {
   const { favorites, addToFavorites, removeFromFavorites } =
     useContext(FavoritesContext);
+  console.log("FAVORITES:\n", favorites);
+  const isFavorite0 = favorites.find((e) => e.uid === event.uid);
+  const isFavorite = () => {
+    favorites ? favorites.find((e) => e.uid === event.uid) : false;
+  };
+
 
   return (
-    <FavoriteButton>
-      <AntDesign name="heart" size={24} color="red" />
+    <FavoriteButton
+      onPress={() =>
+        !isFavorite ? addToFavorites(event) : removeFromFavorites(event)
+      }
+    >
+      <AntDesign
+        name={isFavorite ? "heart" : "hearto"}
+        size={24}
+        color={isFavorite ? "red" : "white"}
+      />
     </FavoriteButton>
   );
 };
