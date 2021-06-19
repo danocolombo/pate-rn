6aa3cf9e1f44586f1f2b20d913d8e1394edb0ebb
danocolombo@google.com <GR0mans1212!>
2021-06-18 21:01:23 -0400

    forgot a file

--- src/infrastructure/navigation/index.js
+++ src/infrastructure/navigation/index.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 
 import { AppNavigator } from "./app.navigator";
-import { AccountNavigator } from "./account.navigator";
+// import { AccountNavigator } from "./account.navigator";
 
 import { AuthenticationContext } from "../../services/authentication/authentication.context";
 
