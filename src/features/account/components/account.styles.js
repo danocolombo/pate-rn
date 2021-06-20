85722275c81750e898e78a9567cc14b135450b36
danocolombo <danocolombo@gmail.com>
2021-06-20 06:49:42 -0400

    adding overlay

--- src/features/account/components/account.styles.js
+++ src/features/account/components/account.styles.js
@@ -7,3 +7,10 @@ export const AccountBackground = styled.ImageBackground.attrs({
   align-items: center;
   justify-content: center;
 `;
+
+export const AccountCover = styled.view`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(255, 255, 255, 0.3);
+`;
