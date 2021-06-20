fc3a0d0778ef3a1c86e4b40f2db5f08b92b1d5a8
danocolombo <danocolombo@gmail.com>
2021-06-20 13:16:08 -0400

    account screen with buttons that go to register and login

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,10 +1,34 @@
 import React from "react";
-import { AccountBackground, AccountCover } from "../components/account.styles";
+import {
+  AccountBackground,
+  AccountContainer,
+  AccountCover,
+} from "../components/account.styles";
+import { Spacer } from "../../../components/spacer/spacer.component";
+import { AuthButton } from "../components/account.styles";
 
-export const AccountScreen = () => {
+export const AccountScreen = ({ navigation }) => {
   return (
     <AccountBackground>
       <AccountCover />
+      <AccountContainer>
+        <AuthButton
+          icon="lock-open-outline"
+          mode="contained"
+          onPress={() => navigation.navigate("Login")}
+        >
+          LOGIN
+        </AuthButton>
+        <Spacer size="large">
+          <AuthButton
+            icon="lock-open-outline"
+            mode="contained"
+            onPress={() => navigation.navigate("Register")}
+          >
+            REGISTER
+          </AuthButton>
+        </Spacer>
+      </AccountContainer>
     </AccountBackground>
   );
 };
