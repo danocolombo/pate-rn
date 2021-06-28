4a272167bbb66314c16f54e20e9c2208f26b0c91
danocolombo@google.com <GR0mans1212!>
2021-06-27 22:44:05 -0400

    settings page with registtrations and logout

--- src/infrastructure/navigation/app.navigator.js
+++ src/infrastructure/navigation/app.navigator.js
@@ -2,18 +2,19 @@ import React from "react";
 import { MaterialIcons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { EventsNavigator } from "../navigation/events.navigator";
+import { SettingsNavigator } from "../navigation/settings.navigator";
 import { EventsContextProvider } from "../../services/events/events.context";
 
 import { LocationContextProvider } from "../../services/location/location.context";
 import { FavoritesContextProvider } from "../../services/favorites/favorites.context";
-import { Profile } from "../../features/profile/screens/profile.screen";
+import { SettingsScreen } from "../../features/settings/screens/settings.screen";
 //import { SafeArea } from "../../components/utility/safe-area.component";
 import { MapScreen } from "../../features/map/screens/map.screen";
 const Tab = createBottomTabNavigator();
 
-function ProfileScreen() {
-  return <Profile />;
-}
+// function ProfileScreen() {
+//   return <Profile />;
+// }
 
 export const AppNavigator = () => {
   return (
@@ -44,7 +45,7 @@ export const AppNavigator = () => {
           >
             <Tab.Screen name="Events" component={EventsNavigator} />
             <Tab.Screen name="Map" component={MapScreen} />
-            <Tab.Screen name="Profile" component={ProfileScreen} />
+            <Tab.Screen name="Profile" component={SettingsNavigator} />
           </Tab.Navigator>
         </EventsContextProvider>
       </LocationContextProvider>
