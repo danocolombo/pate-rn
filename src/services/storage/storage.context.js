51e1132f8362be91b77a851983a332585947e213
danocolombo <danocolombo@gmail.com>
2021-07-13 17:37:17 -0400

    working with s3 files

--- src/services/storage/storage.context.js
+++ src/services/storage/storage.context.js
@@ -5,7 +5,7 @@ import { Storage } from "aws-amplify";
 export const StorageContext = React.createContext();
 
 export const StorageContextProvider = ({ children }) => {
-  const [eventImages, setEventImages] = useState("");
+  const [eventImages, setEventImages] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   //-----------------------------
@@ -13,15 +13,16 @@ export const StorageContextProvider = ({ children }) => {
   //-----------------------------
   //this can move to services once functioning....
   const getS3EventImages = async () => {
-    let imageKeys = await Storage.list("/");
+    let imageKeys = await Storage.list("");
     imageKeys = await Promise.all(
       imageKeys.map(async (k) => {
         const signedUrl = await Storage.get(k.key);
         return signedUrl;
       })
     );
-    console.log("[--imageKeys--]\n", imageKeys);
+    console.log("[--imageKeys[1]--]\n", imageKeys[1]);
     setEventImages(imageKeys);
+    // console.log("[==eventImages count===] " + eventImages.length);
   };
   useEffect(() => {
     getS3EventImages();
